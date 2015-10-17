9ed951a7d29dc49f32253922396abaf8007b9393
Daniel Falabella <dnlfala@gmail.com>
2015-10-16 22:59:46 -0600

    Finished assignment.

--- js/mainCtrl.js
+++ js/mainCtrl.js
@@ -1,15 +1,47 @@
 var app = angular.module('chatroom');
 
 app.controller('mainCtrl', function($scope, parseService){
-  //In your controller you'll have a getParseData function and a postData function, but should be placed on $scope.
+  //In your controller you'll have a getParseData function and a postData function, but
+  //should be placed on $scope.
 
-  //The getParseData function will call the getData method on the parseService object. You'll then save the result of that request to 
+  //The getParseData function will call the getData method on the parseService object.
+  //You'll then save the result of that request to 
   //your controllers $scope as messages ($scope.messages)
 
+  $scope.messages = [];
+
+  var getParseData = function () {
+    // var request = parseService.getData();
+    // console.log(request);
+    parseService.getData().then(function (response) {
+      response.data.results.forEach(function (item, index) {
+        $scope.messages.push(item);
+      })
+    })
+  }
+
+  getParseData();
+
+  $scope.postData = function (message) {
+    if (!message) return;
+    // return console.log(message);
+    parseService.postData(message).then(function (response) {
+      if (response.status === 201) {
+        var newMessage = {
+          text: response.config.data.text
+        }
+      }
+      $scope.messages.unshift(newMessage);
+      $scope.message = '';
+    })
 
 
-  //The postData function will take whatever the user typed in (hint: look at the html and see what ng-model correlates to on the input box),
-  //pass that text to the postData method on the parseService object which will then post it to the parse backend.
+  }
+
+  //The postData function will take whatever the user typed in (hint: look at the html and see
+  //what ng-model correlates to on the input box),
+  //pass that text to the postData method on the parseService object which will then post it to
+  //the parse backend.
 
 
 
